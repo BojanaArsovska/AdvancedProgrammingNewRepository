ba186d2a3a9b181dba0ffe894745d5ba29d8ee0b
Bojana <36798003+BojanaArsovska@users.noreply.github.com>
2018-09-23 22:58:58 +0200

    Update Identifier.java

--- AP_Assignment1/src/Identifier.java
+++ AP_Assignment1/src/Identifier.java
@@ -5,21 +5,81 @@
 
 class Identifier implements IdentifierInterface {
 	
-	private StringBuilder word;
+	private static final int INITIAL_AMOUNT_OF_ELEMENTS = 1;
+
+    private char[] array;
+    private int amountOfElements;
+    
+    
+	public Identifier() {
+		array = new char[INITIAL_AMOUNT_OF_ELEMENTS];
+		amountOfElements = 0;
+	}
 	
-	public Identifier() {};
+	private void copyElements (char[] dest, char[] src, int amount){
+		for (int i = 0; i < amount; i++) {
+			dest[i] = src[i];
+		}
+	}
 	
-	public Identifier(StringBuilder word) {
-		this.word = word;
+	public Identifier (Identifier src) {
+		array = new char [src.array.length];
+		amountOfElements = src.amountOfElements;
+		copyElements(array, src.array, amountOfElements);
 	}
 	
 	public void init(char a) {
-		
+		array[0] = a;
+		amountOfElements = 1;
 	}
 	
-	public void add(StringBuilder strB)
-
-
+	private void increaseIdentifierSize () {
+		char[] result = new char [2 * array.length];
+		copyElements(result, array, amountOfElements);
+		array = result;		
+	}
 	
+	private boolean isFull() {
+		return amountOfElements == array.length;
+	}
+	
+	public void add(char a) {
+		if(isFull()) {
+			increaseIdentifierSize();
+		}
+		
+		array[amountOfElements++] = a;
+ 	}
+	
+	//should i write a remove method, if so, what should be it's parameters?
 
+	public boolean isEmpty() {
+		return amountOfElements == 0;
+	}
+
+	public int size() {
+		return amountOfElements;
+	}
+
+	public boolean equals(Identifier other) {
+		if (amountOfElements != other.amountOfElements)
+			return false;
+		
+		for (int i = 0; i < amountOfElements; i++) {
+			if (array[i] != other.array[i]) {
+				return false;
+			}	
+		}
+		return true;
+	}
+	
+	public String toString() {
+		String result = "";
+		
+		for(int i=0; i<amountOfElements; i++) {
+			result += array[i];
+		}
+		
+		return "[ " + result + " ]";
+	}
 }
