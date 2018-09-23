1d9f95144f4ea2ff6c856cac9c3e0aa48a4aeb21
Bojana <36798003+BojanaArsovska@users.noreply.github.com>
2018-09-23 23:01:20 +0200

    Update Set.java

--- AP_Assignment1/src/Set.java
+++ AP_Assignment1/src/Set.java
@@ -1,23 +1,154 @@
-//  The sets must have the following properties:
-//	Only identifiers are allowed as elements of a set.
-//	Sets contain a minimum of 0 and a maximum of 20 identifiers.
+class Set implements SetInterface{
+	public static final int MAX_AMOUNT_OF_ELEMENTS = 20;
+	private static final int INITIAL_AMOUNT_OF_ELEMENTS = 0;
 
-//Interface
-// structure NONE
-//specification return booleans if user input acceptable
-//remove cannot remove eelements
-// get returns random becuase there is no order
+    private Identifier[] elements;
+    private int amountOfElements;
+     
+    public Set() {
+    	elements = new Identifier[INITIAL_AMOUNT_OF_ELEMENTS];
+    	amountOfElements = 0;
+    }
+    
+    private void copyElements (Identifier[] dest, Identifier[] src, int amount){
+		for (int i = 0; i < amount; i++) {
+			dest[i] = new Identifier(src[i]);
+		}
+	}
+	
+	public Set (Set src) {
+		elements = new Identifier [src.elements.length];
+		amountOfElements = src.amountOfElements;
+		copyElements(elements, src.elements, amountOfElements);
+	}
+   
+	public void init() {
+		amountOfElements = 0;
+	}
+	
+	private boolean isFull() {
+		return amountOfElements == elements.length;
+	}
+	
+	private void increaseSetSize() {
+		Identifier[] result;
+		
+		if (elements.length == INITIAL_AMOUNT_OF_ELEMENTS) 
+			result = new Identifier[INITIAL_AMOUNT_OF_ELEMENTS + 1];
+		else
+			result = new Identifier[2 * elements.length];
+		
+		copyElements(result, elements, amountOfElements);
+		elements = result;
+	}
 
+	public void add(Identifier identifier) {
+		if (!contains(identifier)) {
+			if(isFull()) {
+				increaseSetSize();
+			}
+			
+			elements[amountOfElements++] = new Identifier(identifier);
+		}
+	}
 
-public class Set {
-	
-	private Identifier[] identifiers = new Identifier[20];
-	
-	public Set(){};
-	
+	public void remove(Identifier identifier) {
+		Set temp = new Set();
+		
+		for(int i=0; i<amountOfElements; i++) {
+			if(elements[i].equals(identifier)) {
+				continue;
+			}
+			temp.add(elements[i]);
+		}
+		
+		elements = temp.elements; //check later
+	}
+		
+	public boolean isEmpty() {
+		return amountOfElements == 0;
+	}
+
+	public int size() {
+		return amountOfElements;
+	}
+
+	public boolean equals(Set other) {
+		if(amountOfElements != other.size())
+			return false;
+		
+		int count = 0;
+		for(int i=0; i<amountOfElements; i++) {
+			if(other.contains(elements[i]))
+				count++;
+		}
+		
+		return count == amountOfElements;
+	}
+
+	public boolean contains(Identifier identifier) {
+		for (int i = 0; i < amountOfElements; i++) {
+			if (elements[i].equals(identifier)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	public Set union(Set other) {
+		Set result = new Set(this);
+		
+		for(int i=0; i<other.size(); i++) {
+			if(!result.contains(other.elements[i]))
+				result.add(other.elements[i]);
+		}
+		
+		return result;
+	}
+
+	public Set difference(Set other) {
+		Set result = new Set(this);
+		
+		for (int i=0; i < other.size(); i++) {
+			if (result.contains(other.elements[i]))
+				result.remove(other.elements[i]);
+		}
+		
+		return result;
+	}
+
+	public Set intersection(Set other) {
+		Set result = new Set(this);
+		
+		for(int i=0; i<other.size(); i++) {
+			if(result.contains(other.elements[i]))
+				result.add(other.elements[i]);
+		}
+		
+		return result;
+	}
+
+	public Set symmetricDifference(Set other) {
+		Set result = this.union(other);
+		Set intersection = this.intersection(other);
+		
+		for(int i=0; i<intersection.size(); i++) {
+			result.remove(intersection.elements[i]);
+		}
+		
+		return result;
+	}
+
+	public String toString() {
+		String result = "{ ";
+		
+		for(int i =0; i<amountOfElements-1; i++) {
+			result += elements[i].toString() + " ";
+		}
+		
+		result += elements[amountOfElements - 1] + " }";
+		
+		return result;
+	}
 }
-	
-	
-	
-
-
